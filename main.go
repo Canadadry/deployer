f114d0b9cdf81de73a3c683b203952b878c245dc
Jérôme MOUREY <jerome@everycheck.fr>
2021-04-16 19:40:43 +0200

    add first deployer function

--- main.go
+++ main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
-	"app/pkg/monkey/repl"
+	"app/pkg/monkey"
+	"app/pkg/monkey/object"
 	"fmt"
 	"os"
 )
+
+var store = map[string]string{}
+var task = map[string]*object.Function{}
+
+func Task(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return &object.Error{Message: fmt.Sprintf("task should have only two parameters,got %d", len(args))}
+	}
+	if args[1].Type() != object.FUNCTION_OBJ {
+		return &object.Error{Message: fmt.Sprintf("task should take a string and a function ,got %s", args[1].Type())}
+	}
+	fn := args[1].(*object.Function)
+	if len(fn.Parameters) != 0 {
+		return &object.Error{Message: fmt.Sprintf("task function argument should have no parameters ,got %d", len(fn.Parameters))}
+	}
+	task[args[0].Inspect()] = fn
+	return object.NULL
+}
+
+func Set(args ...object.Object) object.Object {
+	if len(args) != 2 {
+		return &object.Error{Message: fmt.Sprintf("set should have only two parameters,got %d", len(args))}
+	}
+	store[args[0].Inspect()] = args[1].Inspect()
+	return object.NULL
+}
+
+func Get(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return &object.Error{Message: fmt.Sprintf("get should have only one parameter ,got %d", len(args))}
+	}
+	str, ok := store[args[0].Inspect()]
+	if !ok {
+		return object.NULL
+	}
+	return &object.String{Value: str}
+}
 
 func main() {
 	if err := run(os.Args); err != nil {
@@ -21,5 +59,14 @@ func run(args []string) error {
 	if err != nil {
 		return err
 	}
-	return repl.Start(f)
+	eval := monkey.New(map[string]object.BuiltinFunction{
+		"set":  Set,
+		"get":  Get,
+		"task": Task,
+	})
+	result := eval.Eval(f)
+	if result.Type() == object.ERROR_OBJ {
+		return fmt.Errorf(result.Inspect())
+	}
+	return nil
 }
