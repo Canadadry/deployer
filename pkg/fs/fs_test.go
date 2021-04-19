dfb405ae7f747dcb52e457892416250834ce1085
Jérôme MOUREY <jerome@everycheck.fr>
2021-04-19 09:48:38 +0200

    move all test in a generics test case to allow testing all fs the same way

--- pkg/fs/fs_test.go
+++ pkg/fs/fs_test.go
@@ -6,6 +6,91 @@ import (
 	"testing"
 )
 
+func runAllTestCase(t *testing.T, newFs func() FS) {
+	tests := map[string]func(t *testing.T, fs FS){
+		"TestOpeningNotExistingFile": func(t *testing.T, fs FS) {
+			testOpeningNotExistingFile(t, fs, "not_existing_file")
+		},
+		"TestOpeningExistingFileAndReading": func(t *testing.T, fs FS) {
+			testOpeningExistingFileAndReading(t, fs, "file_with_content", "file content")
+		},
+		"TestOpeningExistingFileAndWriting": func(t *testing.T, fs FS) {
+			testOpeningExistingFileAndWriting(t, fs, "empty_file", "file content")
+		},
+		"TestOpeningExistingFile_CannotReadAfterClose": func(t *testing.T, fs FS) {
+			testOpeningExistingFile_CannotReadAfterClose(t, fs, "empty_file")
+		},
+		"TestOpeningExistingFile_CannotWriteAfterClose": func(t *testing.T, fs FS) {
+			testOpeningExistingFile_CannotWriteAfterClose(t, fs, "empty_file")
+		},
+		"TestOpeningExistingFile_CannotCloseTwice": func(t *testing.T, fs FS) {
+			testOpeningExistingFile_CannotCloseTwice(t, fs, "empty_file")
+		},
+		"Test_CanOpenTwiceAFile": func(t *testing.T, fs FS) {
+			testOpeningExistingFile_CannotReadAfterClose(t, fs, "file_with_content")
+			testOpeningExistingFileAndReading(t, fs, "file_with_content", "file content")
+		},
+		"Test_CanReadAfterWrite": func(t *testing.T, fs FS) {
+			testOpeningExistingFile_CannotReadAfterClose(t, fs, "file_with_content")
+			testOpeningExistingFileAndReading(t, fs, "file_with_content", "file content")
+		},
+		"TestOpeningExistingFile_GetSatIsDir": func(t *testing.T, fs FS) {
+			testOpeningExistingFile_GetSatIsDir(t, fs, "empty_file", false)
+		},
+		"TestDeleteFile": func(t *testing.T, fs FS) {
+			testOpeningExistingFileAndReading(t, fs, "file_with_content", "file content")
+			testDeleteFile(t, fs, "file_with_content")
+			testOpeningNotExistingFile(t, fs, "file_with_content")
+		},
+		"TestCreatingFile": func(t *testing.T, fs FS) {
+			testOpeningNotExistingFile(t, fs, "not_existing_file")
+			testCreateFile(t, fs, "not_existing_file")
+			testOpeningExistingFileAndWriting(t, fs, "not_existing_file", "file content")
+			testOpeningExistingFileAndReading(t, fs, "not_existing_file", "file content")
+		},
+		"TestCreatingFile_WhileFileExist_TruncateIt": func(t *testing.T, fs FS) {
+			testOpeningExistingFileAndReading(t, fs, "file_with_content", "file content")
+			testCreateFile(t, fs, "file_with_content")
+			testOpeningExistingFileAndReading(t, fs, "file_with_content", "")
+		},
+		"TestCreatingDirectory": func(t *testing.T, fs FS) {
+			testCreatingDirectory(t, fs, "not_exisiting_dir")
+		},
+		"TestOpeningDirectory": func(t *testing.T, fs FS) {
+			testCreatingDirectory(t, fs, "not_exisiting_dir")
+			testOpeningDirectory(t, fs, "not_exisiting_dir")
+		},
+		"TestCreatingDirectoryOnExistingFile": func(t *testing.T, fs FS) {
+			testCreateFile(t, fs, "not_exisiting_dir")
+			testCreatingDirectoryOnExistingFile(t, fs, "not_exisiting_dir")
+		},
+		"TestReadDir": func(t *testing.T, fs FS) {
+			testCreatingDirectory(t, fs, "not_exisiting_dir")
+			testReadDir(t, fs, "not_exisiting_dir", map[string]bool{})
+			testCreateFile(t, fs, "not_exisiting_dir/not_exisiting_file")
+			testReadDir(t, fs, "not_exisiting_dir", map[string]bool{"not_exisiting_file": false})
+			testCreatingDirectory(t, fs, "not_exisiting_dir/not_exisiting_dir")
+			testReadDir(t, fs, "not_exisiting_dir", map[string]bool{"not_exisiting_file": false, "not_exisiting_dir": true})
+			testCreateFile(t, fs, "not_exisiting_dir/not_exisiting_dir/real_file")
+			testReadDir(t, fs, "not_exisiting_dir", map[string]bool{"not_exisiting_file": false, "not_exisiting_dir": true})
+		},
+		"TestCannotReadFromDirectory": func(t *testing.T, fs FS) {
+			testCreatingDirectory(t, fs, "not_exisiting_dir")
+			testCannotReadFromDirectory(t, fs, "not_exisiting_dir")
+		},
+		"TestCannotWriteInDirectory": func(t *testing.T, fs FS) {
+			testCreatingDirectory(t, fs, "not_exisiting_dir")
+			testCannotWriteInDirectory(t, fs, "not_exisiting_dir")
+		},
+	}
+
+	for title, tt := range tests {
+		t.Run(title, func(t *testing.T) {
+			tt(t, newFs())
+		})
+	}
+}
+
 func testOpeningNotExistingFile(t *testing.T, fs FS, name string) {
 	f, err := fs.Open(name)
 	if f != nil {
