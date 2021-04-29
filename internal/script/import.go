95a1ca5e76f511849fab385b24e83991ac249142
Jérôme MOUREY <jerome@everycheck.fr>
2021-04-29 16:46:34 +0200

    can load from local folder

--- internal/script/import.go
+++ internal/script/import.go
@@ -4,6 +4,7 @@ import (
 	"app/pkg/monkey"
 	"app/pkg/monkey/object"
 	"embed"
+	"os"
 	"path/filepath"
 )
 
@@ -13,7 +14,10 @@ var content embed.FS
 func Import(filename string, eval monkey.Evaluator) object.Object {
 	file, err := content.Open(filepath.Clean("embed/" + filename + ".mky"))
 	if err != nil {
-		return object.FromError(err)
+		file, err = os.Open(filepath.Clean(filename + ".mky"))
+		if err != nil {
+			return object.FromError(err)
+		}
 	}
 	defer file.Close()
 	return eval.Eval(file)
